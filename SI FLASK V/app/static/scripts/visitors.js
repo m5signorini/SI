834371e0a922ee4027ca9f8b983de313b35941e2
Martin Sanchez Signorini <martin.sanchezs@estudiante.uam.es>
2021-10-12 17:52:33 +0200

    error handling num_visitors

--- SI FLASK V/app/static/scripts/visitors.js
+++ SI FLASK V/app/static/scripts/visitors.js
@@ -2,13 +2,23 @@
 
 $('document').ready(visitors_init);
 
+var interv;
+
 function visitors_init() {
-    setInterval(update_visitors, 3000);
+    interv = setInterval(update_visitors, 3000);
+    update_visitors();
 }
 
 function update_visitors() {
-    $.get('/num_visitors', response => {
-        // Success callback:
-        $('#num_visitors').text('Número de visitantes: ' + response);
-    })
+    $.ajax({
+        url: '/num_visitors',
+        type: 'GET',
+        success: function(response){
+            $('#num_visitors').text('Número de visitantes: ' + response);
+        },
+        error: function(data) {
+            console.log('Error al obtener número de visitas, desactivando intervalo');
+            clearInterval(interv);
+        }
+    });
 }
