148267ff96846f2702270d1acc7bb255312c94ed
Martin Sanchez Signorini <martin.sanchezs@estudiante.uam.es>
2021-10-12 17:01:51 +0200

    Sign up updated

--- SI FLASK V/app/static/scripts/signup.js
+++ SI FLASK V/app/static/scripts/signup.js
@@ -7,6 +7,10 @@ function init() {
     $('.user_form').on('submit', validate_form);
     $('#signpassword').on('input', password_input);
     $('#confsignpassword').on('input', confirm_input);
+    $('#signusername').on('input', username_input);
+    $('#email').on('input', email_input);
+    $('#address').on('input', address_input);
+    $('#payment').on('input', payment_input);
 }
 
 function validate_form(e) {
@@ -26,6 +30,18 @@ function validate_form(e) {
         set_span_message($('#submit_result'), 'Correo no válido', 'red');
         return false;
     }
+    if(!validate_username()) {
+        set_span_message($('#submit_result'), 'Nombre de usuario al menos 6 caracteres', 'red');
+        return false;
+    }
+    if(!validate_address()) {
+        set_span_message($('#submit_result'), 'Dirección de entrega máximo 50 caracteres, no especiales', 'red');
+        return false;
+    }
+    if(!validate_payment()) {
+        set_span_message($('#submit_result'), 'Número de tarjeta de crédito no válido (16 digitos)', 'red');
+        return false;
+    }
 }
 
 function validate_email() {
@@ -33,6 +49,27 @@ function validate_email() {
     let regex = /^([a-z]|[A-Z]|[0-9]|_)+@[a-zA-Z0-9_]+\.[a-z]+$/gm;
     return regex.test(mail);
 }
+
+function validate_username() {
+    let name = $('#signusername').val();
+    let regex = /^([a-zA-Z0-9]){6,}$/gm;
+    return regex.test(name);
+}
+
+function validate_address() {
+    let address = $('#address').val();
+    let regex = /^(.){1,50}$/gm;
+    return regex.test(address);
+}
+
+function validate_payment() {
+    let payment = $('#payment').val();
+    let regex = /^([0-9]){16,16}$/gm;
+    return regex.test(payment);
+}
+
+
+
 
 /**
 Funcion de callback para el evento de modificar la entrada de la contraseña para
@@ -45,26 +82,56 @@ function password_input(e) {
     let mens, color;
     if(stren > 3) {
         if(stren > 6) {
-            mens = 'Contraseña segura';
+            mens = 'Contraseña fuerte';
             color = 'green';
         }
         else {
-            mens = 'Contraseña intermedia';
+            mens = 'Contraseña media';
             color = 'orange';
         }
     }
     else {
-        mens = 'Contraseña insegura';
+        mens = 'Contraseña débil';
         color = 'red';
     }
     set_span_message($('#strength_1'), mens, color);
     return;
 }
 
+function confirm_input(e) {
+    let target = $(e.target);
+    let value = target.val();
+    let mens = 'Contraseña incorrecta, debe ser la misma';
+    let color = 'red';
+    // Comparamos con password
+    if(value == $('#signpassword').val()) {
+        mens = 'Contraseña correcta, es la misma';
+        color = 'green';
+    }
+    set_span_message($('#strength_3'), mens, color);
+    return;
+}
+
+function username_input(e) {
+    set_border_color_by_test($(e.target), validate_username());
+}
+function email_input(e) {
+    set_border_color_by_test($(e.target), validate_email());
+}
+function payment_input(e) {
+    set_border_color_by_test($(e.target), validate_payment());
+}
+function address_input(e) {
+    set_border_color_by_test($(e.target), validate_address());
+}
+
+/**
+FUNCIONES AUXILIARES
+*/
 function get_password_strength(password) {
-    let tests = [/[0-9]+/,/[a-z]+/,/[A-Z]+/,/.{6,}/,/.{7,}/,/.{8,}/,/.{9,}/,/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]+/];
+    let tests = [/[0-9]+/,/[a-z]+/,/[A-Z]+/,/.{9,}/,/.{10,}/,/.{11,}/,/.{12,}/,/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]+/];
     let stren = 0;
-    if(password.length < 6) {
+    if(password.length < 8) {
         return 0;
     }
     tests.forEach(t => {
@@ -80,17 +147,11 @@ function set_span_message(element, message, color) {
     element.css({'color':color});
 }
 
-
-function confirm_input(e) {
-    let target = $(e.target);
-    let value = target.val();
-    let mens = 'Contraseña incorrecta, debe ser la misma';
-    let color = 'red';
-    // Comparamos con password
-    if(value == $('#signpassword').val()) {
-        mens = 'Contraseña correcta';
-        color = 'green';
+function set_border_color_by_test(element, test) {
+    if(test == true) {
+        element.css({'border-color':'green'});
     }
-    set_span_message($('#strength_3'), mens, color);
-    return;
+    else {
+        element.css({'border-color':'red'});
+    }
 }
