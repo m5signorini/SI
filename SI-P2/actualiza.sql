133f9bc7cb9288d6378dd96176d0d4c79134aa4c
mssignorini <mssignorini@gmail.com>
2021-11-04 16:32:56 +0100

    Procedure and duplicates sql

--- SI-P2/actualiza.sql
+++ SI-P2/actualiza.sql
@@ -1,11 +1,28 @@
+
+-- ELIMINAR DUPLICADOS DE ORDERDETAIL AGRUPANDO CANTIDADES
+SELECT DISTINCT orderid, prod_id, SUM(quantity) AS quantity, AVG(price) AS price
+INTO duplicates_orderdetail
+FROM orderdetail
+    GROUP BY (orderid, prod_id)
+    HAVING COUNT((orderid, prod_id)) > 1;
+
+DELETE FROM orderdetail
+    WHERE (orderid, prod_id)
+    IN (SELECT orderid, prod_id FROM duplicates_orderdetail);
+
+INSERT INTO orderdetail
+SELECT *
+FROM duplicates_orderdetail;
+
+DROP TABLE duplicates_orderdetail;
+
 -- ORDER
 ALTER TABLE orders ADD FOREIGN KEY (customerid) REFERENCES customers(customerid);
 
 --ORDERDETAIL
 ALTER TABLE orderdetail ADD FOREIGN KEY (orderid) REFERENCES orders(orderid);
 ALTER TABLE orderdetail ADD FOREIGN KEY (prod_id) REFERENCES products(prod_id);
-DELETE FROM orderdetail WHERE 
-ALTER TABLE orderdetail ADD CONSTRAINT PK_orderdetail PRIMARY KEY (orderid,prod_id);
+ALTER TABLE orderdetail ADD CONSTRAINT PK_orderdetail PRIMARY KEY (orderid, prod_id);
 
 --INVENTORY
 ALTER TABLE inventory ADD FOREIGN KEY (prod_id) REFERENCES products(prod_id);
@@ -16,8 +33,14 @@ ALTER TABLE imdb_actormovies ADD FOREIGN KEY (movieid) REFERENCES imdb_movies(mo
 ALTER TABLE imdb_actormovies ADD CONSTRAINT PK_actormovies PRIMARY KEY (actorid,movieid);
 
 
-
-
+-- SET INITIAL CUSTOMER BALANCE
+CREATE OR REPLACE PROCEDURE setCustomersBalance (IN maxBalance bigint)
+    AS  $$
+        BEGIN
+        	UPDATE customers SET balance = floor(random() * (maxBalance + 1))::int;
+        END
+        $$
+    LANGUAGE plpgsql;
 
 
 
