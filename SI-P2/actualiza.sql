aa730b0905622646d486c5cfec79b92b34dfbb0b
juancavr <juncavr@gmail.com>
2021-11-15 22:13:03 +0100

    Orderdetail trigger

--- SI-P2/actualiza.sql
+++ SI-P2/actualiza.sql
@@ -155,34 +155,6 @@ FROM imdb_moviecountries as old_tab JOIN imdb_countries as new_tab ON old_tab.co
 DROP TABLE imdb_moviegenres;
 DROP TABLE imdb_movielanguages;
 DROP TABLE imdb_moviecountries;
-
---QUERIES FOR INSERTING,UPDATING AND DELETING ORDERDETAIL
-CREATE OR REPLACE FUNCTION tr_update_orders() RETURNS trigger AS $$
-        BEGIN
-			IF (TG_OP = 'INSERT') THEN
-				UPDATE orders
-				set netamount = netamount+(NEW.price * NEW.quantity),
-							totalamount = totalamount+((NEW.price * NEW.quantity)/tax+(NEW.price * NEW.quantity))
-				where orders.orderid = new.orderid;
-			ELSIF (TG_OP = 'UPDATE') THEN
-				UPDATE orders
-				set netamount = netamount+((NEW.price * NEW.quantity)-(OLD.price * OLD.quantity)),
-							totalamount = totalamount+(((NEW.price * NEW.quantity)-(OLD.price * OLD.quantity))/tax+((NEW.price * NEW.quantity)-(OLD.price * OLD.quantity)))
-				where orders.orderid = new.orderid;
-			ELSE
-				UPDATE orders
-				set netamount = netamount-(OLD.price * OLD.quantity),
-							totalamount = totalamount-((OLD.price * OLD.quantity)/tax+(OLD.price * OLD.quantity))
-				where orders.orderid = OLD.orderid;
-			END IF;
-
-			return NEW;
-        END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER INS_ORDERDETAIL AFTER INSERT OR DELETE OR UPDATE
-ON orderdetail FOR EACH ROW
-EXECUTE PROCEDURE tr_update_orders();
 -- QUERIES UTILIZADAS EN PRUEBAS
 
 --SELECT actormovies1.actorid , actormovies1.movieid FROM imdb_actormovies AS actormovies1 JOIN imdb_actormovies AS actormovies2
