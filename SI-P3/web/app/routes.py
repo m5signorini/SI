da3b4271e6d527fdb119abca9e681f6b61032c50
mssignorini <mssignorini@gmail.com>
2021-12-06 23:23:40 +0100

    Tablas ej1 fin

--- SI-P3/web/app/routes.py
+++ SI-P3/web/app/routes.py
@@ -31,5 +31,38 @@ def borraCiudad():
 @app.route('/topUK', methods=['POST','GET'])
 def topUK():
     # TODO: consultas a MongoDB ...
-    movies=[[],[],[]]
+    movies = [[], [], []]
+    topUK = database.getMongoCollection(database.mongo_client)
+    # SCI-FI 1994-1998
+    results = topUK.find(
+        {
+            'genres': 'Sci-Fi',
+            'year': {'$gte': 1994, '$lte': 1998}
+        }
+    )
+    movies[0] = list(results)
+
+    # DRAMAS 1998 : ***, The
+    results = topUK.find(
+        {
+            'genres': 'Drama',
+            'year': 1998,
+            'title': {
+                '$regex': '.*, The$'
+            }
+        }
+    )
+    movies[1] = list(results)
+    print(movies[0])
+
+    # Julia Roberts & Alec Baldwin
+    results = topUK.find(
+        {
+            'actors': {
+                '$all': ['Roberts, Julia', 'Baldwin, Alec']
+            }
+        }
+    )
+    movies[2] = list(results)
+
     return render_template('topUK.html', movies=movies)
