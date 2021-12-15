6b822d17cd10a7fbbdcccc7c34843df27754d6e2
mssignorini <mssignorini@gmail.com>
2021-12-15 19:08:49 +0100

    Apartado 3 - Parte 1: Fin (?)

--- SI-P3/web/app/database.py
+++ SI-P3/web/app/database.py
@@ -3,6 +3,7 @@
 import os
 import sys, traceback, time
 
+import sqlalchemy as sqla
 from sqlalchemy import create_engine
 from pymongo import MongoClient
 
@@ -47,21 +48,23 @@ def delCity(city, bFallo, bSQL, duerme, bCommit):
         # (?) Eliminar constraint ON DELETE CASCADE de los pedidos
         db_conn = dbConnect()
         db_conn.execution_options(autocommit=False)
-        # Ejecutar con SQL
-        if bSQL:
-            db_conn.execute((
-                'BEGIN;'
-            ))
-        # Ejecutar con SQLAlchemy
-        else:
-            trans = db_conn.begin()
+        trans = comenzar_transaccion(db_conn, bSQL, dbr)
 
         if not bFallo:
             borrar_orderdetails(db_conn, city, dbr)
+            if bCommit:
+                hacer_commit(db_conn, trans, bSQL, dbr)
+                # BEGIN para poder iniciar transaccion pues commit la acaba
+                trans = comenzar_transaccion(db_conn, bSQL, dbr)
+
             borrar_orders(db_conn, city, dbr)
             borrar_customers(db_conn, city, dbr)
         else:
             borrar_orderdetails(db_conn, city, dbr)
+            if bCommit:
+                hacer_commit(db_conn, trans, bSQL, dbr)
+                # BEGIN para poder iniciar transaccion pues commit la acaba
+                trans = comenzar_transaccion(db_conn, bSQL, dbr)
             borrar_customers(db_conn, city, dbr)
             borrar_orders(db_conn, city, dbr)
     except Exception as e:
@@ -72,51 +75,86 @@ def delCity(city, bFallo, bSQL, duerme, bCommit):
             ))
         else:
             trans.rollback()
-        dbr.append(f'-- Excepcion {e} ocurrida')
-        dbr.append('-- Rollback al comienzo')
+        dbr.append(f'-- ERROR: Excepcion {e} ocurrida')
+        dbr.append('-- Accion: Rollback realizado')
     else:
         # TODO: confirmar cambios si todo va bien
-        if bSQL:
-            db_conn.execute((
-                'COMMIT;'
-            ))
-        else:
-            trans.commit()
-        dbr.append('-- Ejecucion correcta')
-        dbr.append('-- Commit realizado')
+        hacer_commit(db_conn, trans, bSQL, dbr)
+        dbr.append('-- OK: Ejecucion correcta')
 
     return dbr
 
 
+def comenzar_transaccion(connection, bSQL, dbr):
+    # Ejecutar con SQL
+    trans = None
+    if bSQL:
+        connection.execute((
+            'BEGIN;'
+        ))
+    # Ejecutar con SQLAlchemy
+    else:
+        trans = connection.begin()
+    dbr.append('-- Comienzo de transaccion')
+    return trans
+
+
 def borrar_orderdetails(db_conn, city, dbr):
-    db_conn.execute((
-            'DELETE FROM orderdetail '
+    try:
+        db_conn.execute((
+                'DELETE FROM orderdetail '
+                '   WHERE orderid IN '
+                '   (SELECT orderid '
+                '    FROM orders AS od '
+                '    JOIN customers AS ct '
+                '    ON od.customerid = ct.customerid '
+                f'   AND ct.city = \'{city}\'); '
+        ))
+        dbr.append('-- OK: Eliminadas orderdetails correspondientes ')
+    except Exception as e:
+        dbr.append('-- ERROR: Eliminacion de orderdetails ')
+        raise e
+
+
+def borrar_orders(db_conn, city, dbr):
+    try:
+        db_conn.execute((
+            'DELETE FROM orders '
             '   WHERE orderid IN '
             '   (SELECT orderid '
             '    FROM orders AS od '
             '    JOIN customers AS ct '
             '    ON od.customerid = ct.customerid '
-            f'   AND ct.city = \'{city}\'); '
-    ))
-    dbr.append('-- Eliminadas orderdetail ')
-
-
-def borrar_orders(db_conn, city, dbr):
-    db_conn.execute((
-        'DELETE FROM orders '
-        '   WHERE orderid IN '
-        '   (SELECT orderid '
-        '    FROM orders AS od '
-        '    JOIN customers AS ct '
-        '    ON od.customerid = ct.customerid '
-        f'   AND ct.city = \'{city}\');'
-    ))
-    dbr.append('-- Eliminadas orders ')
+            f'   AND ct.city = \'{city}\');'
+        ))
+        dbr.append('-- Eliminadas orders correspondientes ')
+    except Exception as e:
+        dbr.append('-- ERROR: Eliminacion de orders ')
+        raise e
 
 
 def borrar_customers(db_conn, city, dbr):
-    db_conn.execute((
-            'DELETE FROM customers '
-            f'   WHERE city = \'{city}\'; '
-        ))
-    dbr.append('-- Eliminadas customers ')
+    try:
+        db_conn.execute((
+                'DELETE FROM customers '
+                f'   WHERE city = \'{city}\'; '
+            ))
+        dbr.append(f'-- OK: Eliminados customers con ciudad {city}')
+    except Exception as e:
+        dbr.append('-- ERROR: Eliminacion de customers ')
+        raise e
+
+
+def hacer_commit(conn, trans, bSQL, dbr):
+    try:
+        if bSQL:
+            conn.execute((
+                'COMMIT;'
+            ))
+        else:
+            trans.commit()
+        dbr.append('-- OK: Commit realizado con existo')
+    except Exception as e:
+        dbr.append('-- ERROR: Commit ha fallado')
+        dbr.append(f'-- Motivo: {e}')
+    
