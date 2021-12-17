50c9352162ffc31934839ebe6308668c59b1b442
juancavr <juncavr@gmail.com>
2021-12-17 15:33:57 +0100

    Ejercicio 2 codigo acabado

--- SI-P3/sql/countStatus.sql
+++ SI-P3/sql/countStatus.sql
@@ -18,9 +18,84 @@ select count(*)
 from orders
 where status ='Processed';
 
+-- Explain of the two first examples without analyze and indexes
+-- Exampl 1
+explain select count(*)
+from orders
+where status is null;
+
+--Example 2
+explain select count(*)
+from orders
+where status ='Shipped';
+
 -- Index to compare de results of the explain, with and without it
-drop index idx_orders_status;
 create index idx_orders_status on orders(status);
 
--- Llamada a la sentencia analyze
+-- Exampl 1
+explain select count(*)
+from orders
+where status is null;
+
+--Example 2
+explain select count(*)
+from orders
+where status ='Shipped';
+
+drop index idx_orders_status;
+
+-- Since this point every execution of explain will be different than usual, now we are giving extra information
+-- in the form of statistics to the plannifier
 analyze orders;
+
+-- Explain of the two first examples without analyze and indexes
+-- Exampl 1
+explain select count(*)
+from orders
+where status is null;
+
+--Example 2
+explain select count(*)
+from orders
+where status ='Shipped';
+
+-- Index to compare de results of the explain, with and without it
+create index idx_orders_status on orders(status);
+
+-- Exampl 1
+explain select count(*)
+from orders
+where status is null;
+
+--Example 2
+explain select count(*)
+from orders
+where status ='Shipped';
+
+drop index idx_orders_status;
+
+
+-- Explain over the last two examples without indexes
+-- Example 3
+explain select count(*)
+from orders
+where status ='Paid';
+
+-- Example 4
+explain select count(*)
+from orders
+where status ='Processed';
+-- Explain over the las two examples with index over the orders.status attribute
+create index idx_orders_status on orders(status);
+
+-- Example 3
+explain select count(*)
+from orders
+where status ='Paid';
+
+-- Example 4
+explain select count(*)
+from orders
+where status ='Processed';
+
+drop index idx_orders_status;
